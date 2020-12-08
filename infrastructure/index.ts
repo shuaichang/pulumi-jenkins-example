c24b3160df6f577ad8291608b9a63a7bea4dc70b
hryang <yangzhuodog1982@gmail.com>
2020-12-08 09:17:12 +0800

    Add prod stack.

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,13 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as alicloud from "@pulumi/alicloud";
 
-const name = "fc-hz-demo";
-const bucket = new alicloud.oss.Bucket(name);
+// Create an AliCloud resource (OSS Bucket)
+const bucket = new alicloud.oss.Bucket("my-bucket");
 
-const service = new alicloud.fc.Service("pulumi-demo", {
+const service = new alicloud.fc.Service("pulumi-jenkins", {
     logConfig: {
         project: "fc-demo-hangzhou",
         logstore: "demo",
     },
     role: "acs:ram::1986114430573743:role/aliyunfcgeneratedrole-cn-hangzhou-demo",
 })
+
+// Export the name of the bucket
+export const bucketName = bucket.id;
