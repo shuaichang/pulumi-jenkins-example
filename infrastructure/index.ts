11145294e67ede21bb6aada294853713d5a3d2db
hryang <yangzhuodog1982@gmail.com>
2020-12-08 10:40:16 +0800

    Do not set logstore for the service.

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -4,13 +4,8 @@ import * as alicloud from "@pulumi/alicloud";
 // Create an AliCloud resource (OSS Bucket)
 const bucket = new alicloud.oss.Bucket("my-bucket");
 
-const service = new alicloud.fc.Service("pulumi-jenkins", {
-    logConfig: {
-        project: "fc-demo-hangzhou",
-        logstore: "demo",
-    },
-    role: "acs:ram::1986114430573743:role/aliyunfcgeneratedrole-cn-hangzhou-demo",
-})
+const service = new alicloud.fc.Service("pulumi-jenkins");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+export const serviceName = service.id;
